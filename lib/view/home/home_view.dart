208b3f71882eed158bb658b7af37cc3b70dfb179
alper50 <alperyn50@gmail.com>
2022-02-26 12:51:51 +0300

    test

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({ Key? key }) : super(key: key);
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('adsfadsf'),
+      body: TextButton(
+        onPressed: () {},
+        child: Text('Map'),
+      ),
     );
   }
 }
