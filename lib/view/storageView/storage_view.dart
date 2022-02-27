955e39c5996ab704506f0899de337cdf8f3ac6c7
alper50 <alperyn50@gmail.com>
2022-02-27 20:08:34 +0300

    fixes

--- lib/view/storageView/storage_view.dart
+++ lib/view/storageView/storage_view.dart
@@ -1,7 +1,5 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:luggage_storage/product/navigation/route.gr.dart';
 import 'package:luggage_storage/view/bookView/book_view.dart';
 import 'package:transparent_image/transparent_image.dart';
 
